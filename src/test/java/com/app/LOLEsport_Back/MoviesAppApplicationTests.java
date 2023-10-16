30b250ffbea11661a689d9d9ff2c656b237f04da
MaxenceGlt <maxencegirault69@gmail.com>
2023-10-17 01:40:33 +0200

    init project

---
+++ src/test/java/com/app/LOLEsport_Back/MoviesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.LOLEsport_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
